f3ae6e8026c7e1d53a12551e2a698d7008ed0b64
osyoyu <osyoyu@osyoyu.com>
2021-05-06 23:45:26 +0900

    Add tests for non-numbers

--- test/test.rb
+++ test/test.rb
@@ -9,4 +9,8 @@ class TestJapaneseNumbers < MiniTest::Test
   def test_valid_with_units
     assert_equal JapaneseNumbers::Parser.parse("5000兆"), 5000000000000000
   end
+
+  def test_non_numbers
+    assert_equal JapaneseNumbers::Parser.parse("ぜろ"), 0
+  end
 end
